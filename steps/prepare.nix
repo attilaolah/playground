bfb8c324503cfa5d6c8def2b839e368f9fd0935a
Attila Oláh <attilaolah@gmail.com>
2024-05-05 09:09:39 +0200

    Pass GITHUB_OUTPUT via arg.

--- steps/prepare.nix
+++ steps/prepare.nix
@@ -16,8 +16,8 @@ in (pkgs.writeShellScriptBin "prepare" ''
 
   # Export version numbers as output.
   # Allows subsequent steps to use it as input values.
-  ${echo} "v_bldr=${v.bldr}" >> "$OUT"
-  ${echo} "v_pkgs=${v.pkgs}" >> "$OUT"
-  ${echo} "v_toolchain=${v.toolchain}" >> "$OUT"
-  ${echo} "v_tools=${v.tools}" >> "$OUT"
+  ${echo} "v_bldr=${v.bldr}" >> "$1"
+  ${echo} "v_pkgs=${v.pkgs}" >> "$1"
+  ${echo} "v_toolchain=${v.toolchain}" >> "$1"
+  ${echo} "v_tools=${v.tools}" >> "$1"
 '')
