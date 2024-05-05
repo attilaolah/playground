0d38f8d84b177da981e6408b74e5fc8ff6f27294
Attila Oláh <attilaolah@gmail.com>
2024-05-05 10:15:20 +0200

    Add missing binding.

--- steps/bldr-patch-target.nix
+++ steps/bldr-patch-target.nix
@@ -2,6 +2,7 @@
 with pkgs.lib; let
   git = getExe pkgs.git;
   head = getExe' pkgs.coreutils "head";
+  sed = getExe' pkgs.gnused "sed";
 in (pkgs.writeShellScriptBin "bldr-patch-target" ''
   set -euxo pipefail
   cd "$GITHUB_WORKSPACE/bldr"
