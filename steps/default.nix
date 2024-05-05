21a281602fb5dc281826fdcf1d4c9b58eb023549
Attila Oláh <attilaolah@gmail.com>
2024-05-05 10:32:01 +0200

    Rename steps.

--- steps/default.nix
+++ steps/default.nix
@@ -11,8 +11,8 @@
     bldr-build-host = import ./bldr-build-host.nix inputs;
     pkgs-patch-host = import ./pkgs-patch-host.nix inputs;
     pkgs-build-host = import ./pkgs-build-host.nix inputs;
-    bldr-patch-target = import ./bldr-patch-target.nix inputs;
-    bldr-build-target = import ./bldr-build-target.nix inputs;
+    bldr-patch = import ./bldr-patch.nix inputs;
+    bldr-build = import ./bldr-build.nix inputs;
     toolchain-patch = import ./toolchain-patch.nix inputs;
     toolchain-build = import ./toolchain-build.nix inputs;
 
@@ -25,8 +25,8 @@
         bldr-build-host
         pkgs-patch-host
         pkgs-build-host
-        bldr-patch-target
-        bldr-build-target
+        bldr-patch
+        bldr-build
         toolchain-patch
         toolchain-build
       ];
@@ -39,9 +39,9 @@
     bldr-build-host = mkApp {drv = self.packages.${system}.bldr-build-host;};
     pkgs-patch-host = mkApp {drv = self.packages.${system}.pkgs-patch-host;};
     pkgs-build-host = mkApp {drv = self.packages.${system}.pkgs-build-host;};
-    bldr-patch-target = mkApp {drv = self.packages.${system}.bldr-patch-target;};
-    bldr-build-target = mkApp {drv = self.packages.${system}.bldr-build-target;};
-    toolchain-patch-target = mkApp {drv = self.packages.${system}.toolchain-patch;};
-    toolchain-build-target = mkApp {drv = self.packages.${system}.toolchain-build;};
+    bldr-patch = mkApp {drv = self.packages.${system}.bldr-patch;};
+    bldr-build = mkApp {drv = self.packages.${system}.bldr-build;};
+    toolchain-patch = mkApp {drv = self.packages.${system}.toolchain-patch;};
+    toolchain-build = mkApp {drv = self.packages.${system}.toolchain-build;};
   };
 }
